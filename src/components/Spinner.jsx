51a0d1d114ae1997c85b07b5f2cc82b27ea1271c
Roshu00 <urosninkovic@yahoo.com>
2022-01-29 18:59:49 +0100

    updating component

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { BallTriangle } from "react-loader-spinner";
+import { Circles } from "react-loader-spinner";
 
 const Spinner = ({ message }) => {
   return (
-    <div className="flex justify-center items-center w-fill h-full">
-      <BallTriangle />
-      <p className="">{message}</p>
+    <div className="flex flex-col justify-center items-center w-fill h-full gap-5">
+      <Circles color="#3bb3dbda" height={50} width={200} className="m-5" />
+      <p className="text-lg text-center px-2">{message}</p>
     </div>
   );
 };
