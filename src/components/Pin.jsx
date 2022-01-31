011d5168696c593175f261b144ccf610a58c7d6a
Roshu00 <urosninkovic@yahoo.com>
2022-01-31 02:55:41 +0100

    creatinf save pin function

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -1,14 +1,106 @@
-import React from "react";
-import { urlFor } from "../client";
+import React, { useState } from "react";
+import { client, urlFor } from "../client";
+//ROUTER
+import { Link, useNavigate } from "react-router-dom";
+//UUID
+import { v4 as uuidv4 } from "uuid";
+//ICONST
+import { MdDownloadForOffline } from "react-icons/md";
+import { AiTwotoneDelete } from "react-icons/ai";
+import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
+//UTILS
+import { fetchUser } from "../utils/fetchUser";
 
-const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
+const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
+  const [postHovered, setPostHovered] = useState(false);
+  const [savingPost, setSavingPost] = useState(false);
+  const navigate = useNavigate();
+  const user = fetchUser();
+
+  const alreadySaved = !!save?.filter(
+    (item) => item.postedBy._id === user.googleId
+  )?.length;
+  const savePin = (id) => {
+    if (!alreadySaved) {
+      setSavingPost(true);
+      client
+        .patch(id)
+        .setIfMissing({ save: [] })
+        .insert("after", "save[-1]", [
+          {
+            _key: uuidv4(),
+            userId: user.googleId,
+            postedBy: {
+              _type: "postedBy",
+              _ref: user.googleId,
+            },
+          },
+        ])
+        .commit()
+        .then(() => {
+          window.location.reload();
+          setSavingPost(false);
+        });
+    }
+  };
   return (
-    <div>
-      <img
-        src={urlFor(image).width(250).url()}
-        alt="user"
-        className="rounded-lg w-full "
-      />
+    <div className="m-2">
+      <div
+        onMouseEnter={() => {
+          setPostHovered(true);
+        }}
+        onMouseLeave={() => {
+          setPostHovered(false);
+        }}
+        onClick={() => {
+          navigate(`/pin-detail/${_id}`);
+        }}
+        className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
+      >
+        <img
+          src={urlFor(image).width(250).url()}
+          alt="user"
+          className="rounded-lg w-full "
+        />
+        {postHovered && (
+          <div
+            className="absolute top-0 w-full h-hull flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
+            style={{ height: "100%" }}
+          >
+            <div className="flex item-center justify-between">
+              <div className="flex gap-2">
+                <a
+                  href={`${image?.asset?.url}?dl=`}
+                  download
+                  onClick={(e) => e.stopPropagation()}
+                  className="bg-white w-9 h-9 rounded-full flex justify-center items-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+                >
+                  <MdDownloadForOffline />
+                </a>
+              </div>
+              {alreadySaved ? (
+                <button
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white text-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none"
+                >
+                  {save?.lenght} Saved
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white text-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    savePin(_id);
+                  }}
+                >
+                  Save
+                </button>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
