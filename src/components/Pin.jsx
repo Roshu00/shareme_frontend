792b8ae9f332583099d067590a31c6d6c74a8fd2
Roshu00 <urosninkovic@yahoo.com>
2022-02-01 00:30:03 +0100

    fixsing bug

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -83,7 +83,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                   type="button"
                   className="bg-red-500 opacity-70 hover:opacity-100 text-white text-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none"
                 >
-                  {save?.lenght} Saved
+                  {save?.length} Saved
                 </button>
               ) : (
                 <button
