dce2b43bdcc945f6e3b083e124e0ce9bb3db2b7e
Roshu00 <urosninkovic@yahoo.com>
2022-02-02 01:32:57 +0100

    updating component

--- src/components/CreatePin.jsx
+++ src/components/CreatePin.jsx
@@ -22,6 +22,37 @@ const CreatePin = ({ user }) => {
   const [wrongImageType, setWrongImageType] = useState(false);
 
   const navigate = useNavigate();
+
+  const uploadImage = (e) => {
+    const { type, name } = e.target.files[0];
+
+    if (
+      type === "image/png" ||
+      type === "image/svg" ||
+      type === "image/jpeg" ||
+      type === "image/gif" ||
+      type === "image/tiff"
+    ) {
+      setWrongImageType(false);
+      setLoading(true);
+
+      client.assets
+        .upload("image", e.target.files[0], {
+          contentType: type,
+          filename: name,
+        })
+        .then((document) => {
+          setImageAsset(document);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log("Image upload error", error);
+        });
+    } else {
+      setWrongImageType(true);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center flex-col mt-5 lg:h4/5">
       {fields && (
@@ -33,6 +64,83 @@ const CreatePin = ({ user }) => {
         <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
           <div className="flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
             {loading && <Spinner />}
+            {wrongImageType && <p>Wrong image type!</p>}
+            {!imageAsset ? (
+              <label>
+                <div className="flex flex-col items-center justify-center h-full ">
+                  <div className="flex flex-col justify-center items-center">
+                    <p className="font-bold text-2xl">
+                      <AiOutlineCloudUpload />
+                    </p>
+                    <p className="text-lg">Click to upload</p>
+                  </div>
+                  <p className="mt-32 text-gray-400">
+                    Use high-quality JPG, SVG, PNG, GIF, TIFF less than 20MB
+                  </p>
+                </div>
+                <input
+                  type="file"
+                  name="upload-image"
+                  onChange={uploadImage}
+                  className="w-0 h-0"
+                />
+              </label>
+            ) : (
+              <div className="relative h-full">
+                <img
+                  src={imageAsset?.url}
+                  alt="uploaded-pic"
+                  className="h-full w-full"
+                />
+                <button
+                  type="button"
+                  className="absolute bottom-3 right-3 rounded-full p-3 bg-white text-xl cursor-pointer hover-shadow-md outline-none transition-all duration-500 ease-in-out"
+                  onClick={() => {
+                    setImageAsset(null);
+                  }}
+                >
+                  <MdDelete />
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full">
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Add your title"
+            className="outline-none text-xl sm:text3xl font-bold border-b-2 border-gray-200 p-2"
+          />
+          {user && (
+            <div className="flex gap-2 my-2 items-center bg-white rounded-lg">
+              <img
+                src={user.image}
+                alt="user"
+                className="w-10 h-10 rounded-full"
+              />
+              <p className="font-bold">{user.userName}</p>
+            </div>
+          )}
+          <input
+            type="text"
+            value={about}
+            onChange={(e) => setAbout(e.target.value)}
+            placeholder="What is your pin about?"
+            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+          />
+          <input
+            type="text"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            placeholder="Add destination link"
+            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+          />
+          <div className="flex flex-col">
+            <div>
+              <p className="">Chose pin category</p>
+            </div>
           </div>
         </div>
       </div>
