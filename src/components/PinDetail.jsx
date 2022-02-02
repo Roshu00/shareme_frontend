e11f552f8174eac2df7b1f978d71358906a944fb
Roshu00 <urosninkovic@yahoo.com>
2022-02-02 02:14:33 +0100

    updating component

--- src/components/PinDetail.jsx
+++ src/components/PinDetail.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+//ICONST
+import { MdDownloadForOffline } from "react-icons/md";
+//ROUTER
+import { Link, useParams } from "react-router-dom";
+//UUid
+import { v4 as uuidv4 } from "uuid";
+//CLIENT
+import { client, urlFor } from "../client";
+//COMPONENTS
+import Spinner from "./Spinner";
+import MasonryLayout from "./MasonryLayout";
+//UTILS
+import { pinDetailMorePinQuery, pinDetailQuery } from "../utils/data";
 
-const PinDetail = () => {
+const PinDetail = ({ user }) => {
+  const [pins, setPins] = useState(null);
+  const [pinDetail, setPinDetail] = useState(null);
+  const [comment, setComment] = useState("");
+  const [addingComment, setAddingComment] = useState(false);
+  const { pinId } = useParams();
+
+  if (!pinDetail) return <Spinner message="Loading Pin details" />;
+
   return <div>Details</div>;
 };
 
